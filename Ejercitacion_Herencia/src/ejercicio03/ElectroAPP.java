73e1026848fcdf8912a944ca8f0ec17fdf9f493e
Fernando Cabrera <fernando_cabrera2006@hotmail.com>
2021-09-03 09:53:54 -0300

    limpio gitignore para ordenar y luego volver a filtrar

---
+++ Ejercitacion_Herencia/src/ejercicio03/ElectroAPP.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio03;
+
+
+
+/**
+ *
+ * @author Fernando
+ */
+public class ElectroAPP {
+
+    public static void main(String[] args) {
+
+        Lavadora lavadora = new Lavadora();
+
+        System.out.println("-----------LAVADORA-----------");
+        lavadora.crearLavadora();
+
+        System.out.println("Carga: " + lavadora.getCarga());
+        System.out.println("Color: " + lavadora.getColor());
+        System.out.println("Peso: " + lavadora.getPeso());
+        System.out.println("Precio final--> $" + lavadora.precioFinal());
+
+        Televisor televisor = new Televisor();
+
+        System.out.println("-----------TELEVISOR-----------");
+        televisor.crearTelevisor();
+
+        System.out.println("Pulgadas: " + televisor.getResolucion());
+        System.out.println("Color: " + televisor.getColor());
+        System.out.println("TDT: " + televisor.isTdt());
+        System.out.println("Peso: " + televisor.getPeso());
+        System.out.println("Precio final--> $" + televisor.precioFinal());
+
+    }
+
+}
---
+++ Ejercitacion_Herencia/src/ejercicio03/Electrodomestico.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio03;
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Fernando
+ */
+public class Electrodomestico {
+
+    protected double precio;
+    protected String color;
+    protected char consumoEnergetico; //A,B,C,D,E y F
+    protected double peso;
+
+    //constructores
+    public Electrodomestico() {
+    }
+
+    public Electrodomestico(double precio, String color, char consumoEnergetico, double peso) {
+        this.precio = precio;
+        this.color = color;
+        this.consumoEnergetico = consumoEnergetico;
+        this.peso = peso;
+    }
+
+    //métodos
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public char getConsumoEnergetico() {
+        return consumoEnergetico;
+    }
+
+    public void setConsumoEnergetico(char consumoEnergetico) {
+        this.consumoEnergetico = consumoEnergetico;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    private void comprobarConsumoEnergetico(char letra) {
+
+        if (letra == 'A' || letra == 'B' || letra == 'C' || letra == 'D' || letra == 'E') {
+            this.consumoEnergetico = letra;
+        } else {
+            this.consumoEnergetico = 'F';
+        }
+    }
+
+    private void comprobarColor(String color) {
+
+        if (color.equalsIgnoreCase("negro") || color.equalsIgnoreCase("rojo") || color.equalsIgnoreCase("azul") || color.equalsIgnoreCase("gris")) {
+            this.color = color;
+        } else {
+            this.color = "blanco";
+        }
+    }
+
+    public void crearElectrodomestico() {
+
+        Scanner entrada = new Scanner(System.in).useDelimiter("\n");
+
+        this.precio = 1000; //precio de base, 1000
+
+        System.out.println("Ingrese color: ");
+        this.color = entrada.next();
+
+        System.out.println("Indique consumo energetico A ó F: ");
+        this.consumoEnergetico = entrada.next().charAt(0);
+
+        System.out.println("Ingrese peso: ");
+        this.peso = entrada.nextDouble();
+
+        this.comprobarColor(color);
+        this.comprobarConsumoEnergetico(consumoEnergetico);
+
+    }
+
+    public double precioFinal() {
+        double precioFinal = precio;
+
+        switch (consumoEnergetico) {
+            case 'A':
+                precioFinal += 1000;
+                break;
+            case 'B':
+                precioFinal += 800;
+                break;
+            case 'C':
+                precioFinal += 600;
+                break;
+            case 'D':
+                precioFinal += 500;
+                break;
+            case 'E':
+                precioFinal += 300;
+                break;
+            case 'F':
+                precioFinal += 100;
+                break;
+        }
+        if (peso >= 1 && peso <= 19) {
+            precioFinal += 100;
+        }
+        if (peso >= 20 && peso <= 49) {
+            precioFinal += 500;
+        }
+        if (peso >= 50 && peso <= 79) {
+            precioFinal += 800;
+        }
+        if (peso >= 80) {
+            precioFinal += 1000;
+        }
+        System.out.println("precio final Electro: " + precioFinal);
+        return precioFinal;
+    }
+}
